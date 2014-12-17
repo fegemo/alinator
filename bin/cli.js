7002fcd656381c5b83bcfd1a3be5aa118d6c9547
Flavio <fegemo@gmail.com>
2014-12-17 16:29:10 -0200

    logger, cmdline options

--- bin/cli.js
+++ bin/cli.js
@@ -1,9 +1,37 @@
 #!/usr/bin/env node
 
 var commander = require('commander'),
+    updateNotifier = require('update-notifier'),
     open = require('open'),
+    colors = require('colors'),
+    logger = require('../lib/utils/logger'),
+    pkg = require('../package.json'),
     app = require('../lib/app');
 
-app.go(function() {
-  open('file://' + process.cwd() + '/hoje.html');  
+commander
+  .version(pkg.version)
+  .option('-s, --silent', 'Nao escreve nada no console')
+  .parse(process.argv);
+
+if (!commander.silent) {
+  logger.on('log', console.log);
+}
+
+var notifier = updateNotifier({
+  packageName: pkg.name,
+  packageVersion: pkg.version,
+  updateCheckInterval: 1000 * 60  // 1 minute
+});
+
+notifier.notify();
+
+if (notifier.update) {
+  console.log('Uma ' + 'nova versao'.green + '(' + notifier.update.latest + ') esta disponivel!');
+  console.log('Execute o comando abaixo para instalar:');
+  console.log('\t$ npm install -g alinator '.blue);
+}
+
+app.go(function(filePath) {
+  console.log('Abrindo o arquivo: ' + filePath);
+  open('file://' + filePath);
 });
