540cb841609b8db242ebf1548a3e1c52a5b47da5
Flavio Coutinho <fegemo@gmail.com>
2015-05-22 10:22:01 -0300

    Adds option to provide credentials from the CLI
    alinator -u username -p password

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 var commander = require('commander'),
+    // nconf = require('nconf'),
+    fs = require('fs'),
     updateNotifier = require('update-notifier'),
     open = require('open'),
     colors = require('colors'),
@@ -11,11 +13,21 @@ var commander = require('commander'),
 commander
   .version(pkg.version)
   .option('-s, --silent', 'Nao escreve nada no console')
+  .option('-u, --username <v>', 'Define o usuario que sera usado para fazer login no sistema de agendamento (nome_decom)')
+  .option('-p, --password <v>', 'Define a senha que sera usada para fazer login no sistema de agendamento')
   .parse(process.argv);
 
 if (!commander.silent) {
   logger.on('log', console.log);
   logger.on('error', console.error);
+}
+
+var config = require('../config.json');
+config.credentials.user = commander.username || config.credentials.user;
+config.credentials.pwd = commander.password || config.credentials.pwd;
+
+if (!config.credentials.user || !config.credentials.pwd) {
+  commander.help();
 }
 
 var notifier = updateNotifier({
@@ -32,7 +44,7 @@ if (notifier.update) {
   console.log('\t$ npm install -g alinator '.blue);
 }
 
-app.go(function(filePath) {
+app.go(config, function(filePath) {
   console.log('Abrindo o arquivo: ' + filePath);
   open('file://' + filePath);
 });
